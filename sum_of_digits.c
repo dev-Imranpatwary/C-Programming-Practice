d2c25d0d3a86c82923c39524a36246c07d7253f8
dev-Imranpatwary <patwaryimran859@gmail.com>
2026-07-31 21:01:46 +0600

    Update sum_of_digits.c

--- sum_of_digits.c
+++ sum_of_digits.c
@@ -1,1 +1,18 @@
 
+#include<stdio.h>
+int main(){
+int num,digit1, digit1, digit3, digit4, sum;
+   printf("Enter a 4 digit number: ");
+   scanf("%d",&num);
+   digit4= num % 10;
+   num= num / 10;
+   digit3= num % 10;
+   num= num / 10;
+   digit2= num % 10;
+   num= num / 10;
+   digit1= num % 10;
+   num= num / 10;
+   sum= digit1 + digit2 + digit3 + digit4;
+   printf("Sum off all digits:%d",sum);
+return 0;
+}
