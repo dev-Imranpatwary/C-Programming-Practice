e1349b24c5af012dc64f21139b7b90e75e99f87f
dev-Imranpatwary <patwaryimran859@gmail.com>
2026-07-31 21:02:59 +0600

    Implement fractional part extraction from decimal input
    Added functionality to extract and display the fractional part of a decimal number.

--- extract_fraction.c
+++ extract_fraction.c
@@ -1,1 +1,11 @@
-
+#include<stdio.h>
+int main()
+{
+  float num,fractional_part;
+  printf("Enter a decimal number: ");
+  scanf("%f",&num);
+  fractional_part=num-(int)num;
+  printf("value:%d\n",(int)num);
+  printf("extracted value:%f\n",fractional_part);
+return 0;
+}
