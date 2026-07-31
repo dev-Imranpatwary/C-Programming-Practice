2e34a042cc4be023a7935e98b8379bafdfeab06d
dev-Imranpatwary <patwaryimran859@gmail.com>
2026-07-31 21:04:08 +0600

    Implement function to convert negative to positive

--- make_positive.c
+++ make_positive.c
@@ -1,1 +1,9 @@
-
+#include<stdio.h>
+int main(){
+ float num,positive_value;
+ printf("Enter a negative number: ");
+ scanf("%f",&num);
+ positive_value=num*(-1);
+printf("Converted value:%.f\n",positive_value);
+return 0;
+}
